2db4ef46fa5608582693760d42b205555461df1c
Mark Brighton <marik.shevchenko@icloud.com>
2020-11-19 16:03:22 +0200

    Commit changes

--- ToDoForm/Task/Serializer.cs
+++ ToDoForm/Task/Serializer.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Task
 {
@@ -11,29 +13,34 @@ namespace Task
 
         public T Deserialize<T>(string input) where T : class
         {
-
             System.Xml.Serialization.XmlSerializer ser = new System.Xml.Serialization.XmlSerializer(typeof(T));
-            using (StringReader sr = new StringReader(input)
+            
+            using (StringReader sr = new StringReader(input))
             {
-
                 return (T)ser.Deserialize(sr);
-
+            }
         }
 
-    }
-
-    public string Serialize<T>(T ObjectToSerialize)
-    {
-
-        XmlSerializer xmlSerializer = new XmlSerializer(ObjectToSerialize.GetType());
-
-        using (StringWriter textWriter = new StringWriter())
+        public string Serialize<T>(T ObjectToSerialize)
         {
-            xmlSerializer.Serialize(textWriter, ObjectToSerialize);
-            return textWriter.ToString();
+            XmlSerializer xmlSerializer = new XmlSerializer(ObjectToSerialize.GetType());
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, ObjectToSerialize);
+                return textWriter.ToString();
+            }
         }
 
     }
+}
 
-}
-}
+
+
+
+
+
+
+
+
+
