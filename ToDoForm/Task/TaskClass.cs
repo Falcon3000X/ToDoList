1964532244ad4733167ecd379c63351badaa0228
lilyakaras <lilyca.hrupa@gmail.com>
2020-11-19 09:59:35 +0200

    Create two new classes. TaskClass and TaskList.

---
+++ ToDoForm/Task/TaskClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class TaskClass
+    {
+        public int TaskID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime Deadline { get; set; }
+
+        public TaskClass() { }
+        public TaskClass(int id, string title, string desc, DateTime dateTime)
+        {
+            this.TaskID = id;
+            this.Title = title;
+            this.Description = desc;
+            this.Deadline = dateTime;
+        }
+    }
+}
---
+++ ToDoForm/Task/Tasks.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    [TestFixture]
+    public class Tasks
+    {
+        public List<TaskClass> TasksList { get; set; }
+
+        [Test]
+        public void Add(TaskClass task)
+        {
+            TasksList.Add(task);
+        }
+
+        [Test]
+        public void Remove(TaskClass task)
+        {
+            TasksList.Remove(task);
+        }
+
+        [Test]
+        public List<TaskClass> GetAllTasks()
+        {
+            return TasksList;
+        }
+
+    }
+}
